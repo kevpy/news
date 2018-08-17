769cafe0618cf863c54efb7e4a3eb90878b68ef8
kelvin Kibet <kibetrosh@gmail.com>
2018-08-17 14:21:57 +0300

    implement ScanStream Transformer to fetchItem()

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
@@ -5,16 +6,37 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
 
+  Observable<Map<int, Future<ItemModel>>> items;
+  
   // Getters to get Streams
   Observable<List<int>> get topIds => _topIds.stream;
+
+  // Getters to sinks
+  Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    items = _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
     _topIds.sink.add(ids);
   }
 
+  _itemsTransformer() async {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
   dispose() {
     _topIds.close();
+    _items.close();
   }
 }
